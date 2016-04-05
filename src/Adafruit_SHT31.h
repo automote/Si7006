17cd722dd5280a5c33945c196587d7a10fc6a13b
lovelesh patel <lovelesh06101990@gmail.com>
2016-04-05 13:19:07 +0530

    APIs done

--- src/Adafruit_SHT31.h
+++ src/Adafruit_SHT31.h
@@ -1,56 +1,97 @@
-/*************************************************** 
-  This is a library for the SHT31 Digital Humidity & Temp Sensor
+/*
+	Si7006 Temperature and humidity sensor library for Arduino
+	Lovelesh, thingTronics
+	
+The MIT License (MIT)
 
-  Designed specifically to work with the SHT31 Digital sensor from Adafruit
-  ----> https://www.adafruit.com/products/2857
+Copyright (c) 2015 thingTronics Limited
 
-  These displays use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-#if (ARDUINO >= 100)
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-#include "Wire.h"
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
-#define SHT31_DEFAULT_ADDR    0x44
-#define SHT31_MEAS_HIGHREP_STRETCH 0x2C06
-#define SHT31_MEAS_MEDREP_STRETCH  0x2C0D
-#define SHT31_MEAS_LOWREP_STRETCH  0x2C10
-#define SHT31_MEAS_HIGHREP         0x2400
-#define SHT31_MEAS_MEDREP          0x240B
-#define SHT31_MEAS_LOWREP          0x2416
-#define SHT31_READSTATUS           0xF32D
-#define SHT31_CLEARSTATUS          0x3041
-#define SHT31_SOFTRESET            0x30A2
-#define SHT31_HEATEREN             0x306D
-#define SHT31_HEATERDIS            0x3066
+version 0.1
+*/
 
-class Adafruit_SHT31 {
- public:
-  Adafruit_SHT31();
-  boolean begin(uint8_t i2caddr = SHT31_DEFAULT_ADDR);
-  float readTemperature(void);
-  float readHumidity(void);
-  uint16_t readStatus(void);
-  void reset(void);
-  void heater(boolean);
-  uint8_t crc8(const uint8_t *data, int len);
+#ifndef Si7006_h
+#define Si7006_h
 
- private:
-  boolean readTempHum(void);
-  void writeCommand(uint16_t cmd);
+#include "Arduino.h"
 
-  uint8_t _i2caddr;
-  boolean readData(void);
-  float humidity, temp;
+#define Si7006_ADDR   0x40 // default address
+
+// Si7006 register addresses
+#define Si7006_MEAS_REL_HUMIDITY_MASTER_MODE    0xE5
+#define Si7006_MEAS_REL_HUMIDITY_NO_MASTER_MODE 0xF5
+#define Si7006_MEAS_TEMP_MASTER_MODE            0xE3
+#define Si7006_MEAS_TEMP_NO_MASTER_MODE         0xF3
+#define Si7006_READ_TEMP                        0xE0
+#define Si7006_RESET 							0xFE
+#define Si7006_WRITE_HUMIDITY_TEMP				0xE6						
+#define Si7006_READ_HUMIDITY_TEMP				0xE7
+#define Si7006_WRITE_HEATER_CONTR				0x51
+#define Si7006_READ_HEATER_CONTR				0x11
+#define Si7006_READ_ID_LOW_0					0xFA
+#define Si7006_READ_ID_LOW_1					0x0F
+#define Si7006_READ_ID_HIGH_0					0xFC
+#define Si7006_READ_ID_HIGH_1					0xC9
+#define Si7006_FIRMWARE_0						0x84
+#define Si7006_FIRMWARE_1						0xB8
+
+
+class Si7006 {
+	public:
+		Si7006(void);
+			// Si7006 object
+			
+		boolean begin(byte _i2c_address = Si7006_ADDR);
+			// Initialize Si7006 library with default address (0x40)
+			// Always returns true
+			
+		float readTemperature(void);
+			// Returns the temperature from the sensor
+			
+		float readHumidity(void);
+			// Returns the relative humidity from the sensor
+			
+		uint16_t readStatus(void);
+			// Returns the status of the sensor
+			
+		void reset(void);
+			// SW Reset the sensor
+			
+		void heater(boolean);
+			// Initializes the internal heater
+			
+		uint8_t crc8(const uint8_t *data, int len);
+			// Returns the CRC byte generated from the data 
+		
+	private:
+		boolean readTempHum(void);
+			// Reads the temperature and relative humidity from the sensor
+			
+		void writeCommand(uint16_t cmd);
+			// Writes command bytes to sensor
+			
+		boolean readData(void);
+			// Reads data from the sensor
+
+		byte _i2c_address;
+		float humidity, temp;
 };
 
+#endif
