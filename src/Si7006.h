f8231ac5d707c896b7d778cd544d7a040304c685
lovelesh patel <lovelesh06101990@gmail.com>
2016-04-05 16:47:34 +0530

    rewriting APIs done

--- src/Si7006.h
+++ src/Si7006.h
@@ -41,8 +41,8 @@ version 0.1
 #define Si7006_MEAS_TEMP_NO_MASTER_MODE         0xF3
 #define Si7006_READ_TEMP                        0xE0
 #define Si7006_RESET 							0xFE
-#define Si7006_WRITE_HUMIDITY_TEMP				0xE6						
-#define Si7006_READ_HUMIDITY_TEMP				0xE7
+#define Si7006_WRITE_HUMIDITY_TEMP_CONTR		0xE6						
+#define Si7006_READ_HUMIDITY_TEMP_CONTR 		0xE7
 #define Si7006_WRITE_HEATER_CONTR				0x51
 #define Si7006_READ_HEATER_CONTR				0x11
 #define Si7006_READ_ID_LOW_0					0xFA
@@ -62,33 +62,144 @@ class Si7006 {
 			// Initialize Si7006 library with default address (0x40)
 			// Always returns true
 			
-		uint16_t readStatus(void);
-			// Returns the status of the sensor
-			
-		void reset(void);
+		boolean setPowerUp(void);
+			// Turn on LTR303, begin integration
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+
+		boolean setPowerDown(void);
+			// Turn off LTR303
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+		
+		boolean reset(void);
 			// SW Reset the sensor
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
 			
-		void heater(boolean);
-			// Initializes the internal heater
+		boolean getTempControl(byte &res, boolean voltage, boolean heater);
+			// Gets the contents RH/Temp User Register of the sensor
+			// res uses D7 and D0 bit
+			// If res = 0, RH is set to 12 bit & temp 14 bit resolution (default)
+			// If res = 1, RH is set to 8 bit & temp 12 bit resolution
+			// If res = 2, RH is set to 10 bit & temp 13 bit resolution
+			// If res = 4, RH is set to 11 bit & temp 11 bit resolution
+			//----------------------------------------------------------
+			// If voltage = false(0), VDD OK (default)
+			// If voltage = true(1), VDD LOW
+			//----------------------------------------------------------
+			// If heater = false(0), On-chip Heater is disabled (default)
+			// If heater = true(1), On-chip Heater is disabled
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
 			
-		float readTemperature(void);
-			// Returns the temperature from the sensor
+		boolean setTempControl(byte res, boolean voltage = false, boolean heater = false);
+			// Sets the contents RH/Temp User Register of the sensor
+			// Gets the contents RH/Temp User Register of the sensor
+			// res uses D7 and D0 bit
+			// If res = 0, RH is set to 12 bit & temp 14 bit resolution (default)
+			// If res = 1, RH is set to 8 bit & temp 12 bit resolution
+			// If res = 2, RH is set to 10 bit & temp 13 bit resolution
+			// If res = 4, RH is set to 11 bit & temp 11 bit resolution
+			//----------------------------------------------------------
+			// If voltage = false(0), VDD OK (default)
+			// If voltage = true(1), VDD LOW
+			//----------------------------------------------------------
+			// If heater = false(0), On-chip Heater is disabled (default)
+			// If heater = true(1), On-chip Heater is disabled
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
 			
-		float readHumidity(void);
-			// Returns the relative humidity from the sensor
+		boolean getHeaterControl(byte &heaterCurrent);
+			// Gets the Heater current of the On-chip Heater
+			// If heaterCurrent = 0, Heater current is 3.09mA (default)
+			// If heaterCurrent = 15, Heater current is 94.20mA
+			// heaterCurrent is in multiples of 3.09mA
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+			
+		boolean setHeaterControl(byte heaterCurrent);
+			// Sets the Heater current of the On-chip Heater
+			// If heaterCurrent = 0, Heater current is 3.09mA (default)
+			// If heaterCurrent = 15, Heater current is 94.20mA
+			// heaterCurrent is in multiples of 3.09mA
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+		
+
+		boolean getDeviceID(double &deviceID);
+			// Gets the Device ID of the chip
+			// Default value is 0x06
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+
+		boolean getFirmwareVer(byte &firmware);
+			// Gets the Firmware Version of the chip
+			// Default value is 0xFF for version 1.0
+			// or 0x20 for version 2.0
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+			
+		boolean measureTemperature(float &temperature, boolean mode = false);
+			// Gets the Temperature data from the sensor
+			// If mode = false(0), Hold Master Mode is used
+			// If mode = true(1), No Hold Master Mode is used
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+			
+		boolean measureHumidity(float &humidity, boolean mode = false);
+			// Gets the Humidity data from the sensor
+			// If mode = false(0), Hold Master Mode is used
+			// If mode = true(1), No Hold Master Mode is used
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
 			
 		uint8_t crc8(const uint8_t *data, int len);
 			// Returns the CRC byte generated from the data
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() below)
+			
+		byte getError(void);
+			// If any library command fails, you can retrieve an extended
+			// error code using this command. Errors are from the wire library: 
+			// 0 = Success
+			// 1 = Data too long to fit in transmit buffer
+			// 2 = Received NACK on transmit of address
+			// 3 = Received NACK on transmit of data
+			// 4 = Other error
 		
 	private:
-		boolean readTempHum(void);
-			// Reads the temperature and relative humidity from the sensor
-			
-		void writeCommand(uint16_t cmd);
-			// Writes command bytes to sensor
+		
+		boolean readByte(byte address, byte &value);
+			// Reads a byte from a LTR303 address
+			// Address: LTR303 address (0 to 15)
+			// Value will be set to stored byte
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() above)
+	
+		boolean writeByte(byte address, byte value);
+			// Write a byte to a LTR303 address
+			// Address: LTR303 address (0 to 15)
+			// Value: byte to write to address
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() above)
 
+		boolean readUInt(byte address, unsigned int &value);
+			// Reads an unsigned integer (16 bits) from a LTR303 address (low byte first)
+			// Address: LTR303 address (0 to 15), low byte first
+			// Value will be set to stored unsigned integer
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() above)
+
+		boolean writeUInt(byte address, unsigned int value);
+			// Write an unsigned integer (16 bits) to a LTR303 address (low byte first)
+			// Address: LTR303 address (0 to 15), low byte first
+			// Value: unsigned int to write to address
+			// Returns true (1) if successful, false (0) if there was an I2C error
+			// (Also see getError() above)
+		
 		byte _i2c_address;
-		float humidity, temp;
+		byte _error;
 };
 
 #endif
